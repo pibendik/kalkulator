5aeb436812a74599aeadf246df030e00b5605481
Bendik Østrem Svalastog <9394416+pibendik@users.noreply.github.com>
2022-09-21 12:53:47 +0200

    Initial Commit

---
+++ EksempelForGitInit/EksempelForGitInitApp.swift
@@ -0,0 +1,17 @@
+//
+//  EksempelForGitInitApp.swift
+//  EksempelForGitInit
+//
+//  Created by Bendik Svalastog on 21/09/2022.
+//
+
+import SwiftUI
+
+@main
+struct EksempelForGitInitApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
